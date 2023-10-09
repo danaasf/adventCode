54f8f67464518f9bdaa524d6d5c8ba90f453a306
danaasf <danaasfour35@gmail.com>
2023-10-09 19:21:24 +0300

    09102023

--- day1/main.js
+++ day1/main.js
@@ -8,13 +8,22 @@ const rl = readline.createInterface({
 });
 
 let sum = [];
-let elf_sum=0,max=0; 
+let elf_sum=0,max=0;max1=0,max2=0,max3=0; 
 
 rl.on('line', (line) => {
   if(line.length===0){
     sum.push(parseInt(elf_sum));
-    max=Math.max(...sum);
+    max1=Math.max(...sum);
+    let index1= sum.indexOf(max1);
+    sum[index1]=0;
+    max2=Math.max(...sum);
+    let index2= sum.indexOf(max2);
+    sum[index2]=0;
+    max3=Math.max(...sum);
+    sum[index1]=max1;
+    sum[index2]=max2
     elf_sum=0;
+    max=max1+max2+max3;
   }
   else {
     elf_sum+= parseInt(line);
